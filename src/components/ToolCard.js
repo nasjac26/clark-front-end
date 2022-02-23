74cf1f0b17188528e0b86ee7243bdf8d2911a90f
Jacob <Jacobonasrallah@gmail.com>
2022-02-23 11:08:09 -0800

    tweaking snipcart props for heroku test

--- src/components/ToolCard.js
+++ src/components/ToolCard.js
@@ -24,7 +24,7 @@ function ToolCard(props) {
                             data-item-id={props.id}
                             data-item-name={props.name}
                             data-item-price={props.price}
-                            data-item-url={`/${props.id}`}
+                            data-item-url={`/tools/${props.id}`}
                             data-item-description={props.description}>
                                 Add to Cart
                         </Button>
